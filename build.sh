da566fd195d530d141c4699f5472624c8d46ce5f
david chivato de la fuente <105808314+destroyer-dcf@users.noreply.github.com>
2023-09-21 12:37:40 +0200

    Generate Tag

--- build.sh
+++ build.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
-CHANGES=`cat CHANGES`
+
 PROJECT="gotocpc"
 FECHA=$(date '+%Y-%m-%d %H:%M:%S')
+rojo='\033[0;31m'
+reset_color='\033[0m'
+
+generateChanges() {
+    carpeta="VERSIONS"
+    archivo_concatenado="CHANGES"
+    if [ -e "CHANGES" ]; then
+        rm CHANGES
+    fi
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        archivos=$(find "$carpeta" -type f -exec ls -t -p "{}" + | awk '{print $NF}')
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        archivos=$(find "$carpeta" -type f -exec ls -t -p "{}" + | awk '{print $NF}')
+    else
+        echo "Sistema operativo no compatible"
+        exit 1
+    fi
+    for archivo in $archivos; do
+        cat "$archivo" >> "$archivo_concatenado"
+    done
+    echo "Archivos concatenados en $archivo_concatenado\n"
+}
 
 echo ""
 echo "================================================================================================"
@@ -12,7 +34,6 @@ echo ""
 
     directorio_a_eliminar="dist"
     if [ -d "$directorio_a_eliminar" ]; then
-        # El directorio existe, así que lo eliminamos
         rm -r "$directorio_a_eliminar"
         echo "Directorio eliminado: $directorio_a_eliminar"
     else
@@ -20,7 +41,6 @@ echo ""
     fi
     directorio_a_eliminar="$PROJECT.egg-info"
     if [ -d "$directorio_a_eliminar" ]; then
-        # El directorio existe, así que lo eliminamos
         rm -r "$directorio_a_eliminar"
         echo "Directorio eliminado: $directorio_a_eliminar"
     else
@@ -28,18 +48,11 @@ echo ""
     fi
     directorio_a_eliminar="$PROJECT/__pycache__"
     if [ -d "$directorio_a_eliminar" ]; then
-        # El directorio existe, así que lo eliminamos
         echo "Directorio eliminado: $directorio_a_eliminar"
     else
         echo "El directorio no existe: $directorio_a_eliminar"
     fi
 
-echo ""
-echo "================================================================================================"
-echo  "[*] HISTORIAL DE CAMBIOS"
-echo "================================================================================================"
-echo ""
-echo "$CHANGES"
 echo ""
 echo "================================================================================================"
 echo "[*] INSTALACION DE DEPENDENCIAS"
@@ -48,8 +61,16 @@ echo ""
 pip install build
 pip install --upgrade twine
 echo ""
+echo "================================================================================================"
+echo  "[*] HISTORIAL DE CAMBIOS"
+echo "================================================================================================"
+echo ""
 if [ -z "$1" ]
 then
+    generateChanges
+    CHANGES=`cat CHANGES`
+    echo "$CHANGES"
+    echo ""
     archivo="$PROJECT/__init__.py"
     version=$(grep -o "__version__ = '[0-9]\+\.[0-9]\+\.[0-9]\+'" "$archivo")
     version=$(echo "$version" | sed "s/__version__ = '//;s/'//")
@@ -58,13 +79,21 @@ then
     echo "================================================================================================"
     echo ""
 else
-    echo "__version__ = '$1'" > "$PROJECT/__init__.py"
-
+    version=$1
+    version_a_verificar="VERSIONS/$version"
+    if [ -e "$version_a_verificar" ]; then
+        echo "El archivo $version_a_verificar existe."
+        echo ""
+    else
+        echo "${rojo}ERROR: El archivo $version_a_verificar no existe.${reset_color}"
+        exit
+    fi
+    echo "__version__ = '$version'" > "$PROJECT/__init__.py"
     echo "================================================================================================"
     echo "[*] COMPILAMOS SOFTWARE CON NUEVA VERSION $1"
     echo "================================================================================================"
     echo ""
-    version=$1
+   
 fi
 
 python3 -m build
